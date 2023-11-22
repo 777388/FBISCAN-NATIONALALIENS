af61065f6b1a2389156d0276af10dfc026f5d426
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-11-22 03:29:18 -0800

    Update fbiscan.py

--- fbiscan.py
+++ fbiscan.py
@@ -1,6 +1,6 @@
 import sys
 import re
-print("Usage: python3 fbiscan.py 'search term' 'nationalist | fbi'")
+print("Usage: python3 fbiscan.py 'search term' 'nationalist | fbi | Rosdesto | celeb'")
 if (sys.argv[2] == "fbi"):
     with open("namefile FBI.mhtml", "r") as f:
         for line in f:
@@ -14,6 +14,12 @@ if(sys.argv[2] == "nationalist"):
 
 if(sys.argv[2] == "Rosdesto"):
     with open("namelist", "r") as f:
+        for line in f:
+            if re.search(sys.argv[1].lower(), line.lower()):
+                print(line)
+
+if(sys.argv[2] == "celeb"):
+    with open("CELEBRITY ADDRESSES MEGA LIST.txt", "r") as f:
         for line in f:
             if re.search(sys.argv[1].lower(), line.lower()):
                 print(line)
